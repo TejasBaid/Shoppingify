6f56f019b5afcb31dcf8674cd6712ea51bbbaa35
tejasbaid <tejasbaid3@gmail.com>
2020-08-05 20:35:56 +0530

    Item List UI finished

--- src/components/centre/Center.jsx
+++ src/components/centre/Center.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './center.scss'
 import { Header } from '../header/Header'
+import { ItemList } from '../item-list/ItemList'
 
 export const Center = () => {
     return (
         <div className='center'>
             <Header />
+            <ItemList />
         </div>
     )
 }
--- src/components/item-list/ItemList.jsx
+++ src/components/item-list/ItemList.jsx
@@ -3,8 +3,58 @@ import './ItemList.scss'
 
 export const ItemList = () => {
     return (
-        <div>
-            
+        <div className='item-list'>
+            <h3 className="category-title">Fruits and vegetables</h3>
+            <div className="category-grid">
+                <div className="item">
+                    <h3 className='item-header'>Avocado</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Bananas</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Bunch of carrots 5pcs</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Chicken 1kg</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Pre Cooked Corn 450g</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Mandarin Nadorcott</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Watermelon</h3>
+                    <h3 className="add">+</h3>
+                </div>
+            </div>
+            <h3 className="category-title">Meat and fish</h3>
+            <div className="category-grid">
+                <div className="item">
+                    <h3 className='item-header'>Chicken Leg Box</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Chicken 1kg</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Pork fillets 450g</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                <div className="item">
+                    <h3 className='item-header'>Salmon 1kg</h3>
+                    <h3 className="add">+</h3>
+                </div>
+                
+            </div>
         </div>
     )
 }
