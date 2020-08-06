08d9c67caca9cc9861e113240dc701062476e96a
tejasbaid <tejasbaid3@gmail.com>
2020-08-06 15:10:17 +0530

    List bar form UI completed

--- src/components/centre/Center.jsx
+++ src/components/centre/Center.jsx
@@ -3,11 +3,13 @@ import './center.scss'
 import { Header } from '../header/Header'
 import { ItemList } from '../item-list/ItemList'
 
+
 export const Center = () => {
     return (
         <div className='center'>
             <Header />
             <ItemList />
+            
         </div>
     )
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import './homepage.scss'
 import { Sidebar } from '../../components/sidebar/Sidebar'
 import { Center } from '../../components/centre/Center'
+import { ListBar } from '../../components/list-bar/ListBar'
+import {Empty} from '../../components/list-bar/Empty'
+import {ListBarForm} from '../../components/list-bar/ListBarForm'
 
 export const Homepage = () => {
+    var ListBarType = "List"
+    //TODO:Implement context to handle the type of sidebar list
     return (
         <div className='homepage'>
             <Sidebar />
             <Center />
+            <div className="list-bar-container">
+                {ListBarType === "List"
+                    ?( <ListBar /> )
+                    :ListBarType === "Empty"
+                        ?( <Empty /> )
+                        :ListBarType === "Form"
+                            ?(<ListBarForm />)
+                            : null
+                }
+            </div>
         </div>
     )
 }
